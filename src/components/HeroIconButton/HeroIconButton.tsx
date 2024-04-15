86cdffb978f86d14ce552f1b9d276e9cfa4e0262
Rodolfo Pereira <phinpho@gmail.com>
2024-04-15 09:17:41 +0200

    feat: add animation effects to the hero button

--- src/components/HeroIconButton/HeroIconButton.tsx
+++ src/components/HeroIconButton/HeroIconButton.tsx
@@ -2,6 +2,7 @@ import { useMemo, useRef } from "react";
 import { Button, type ButtonComponent, type ButtonProps } from "../Button";
 import { Icon, type IconProps } from "../Icon";
 import { variantBgClass } from "../../lib/variantClasses";
+import { cls } from "../../lib/cls";
 
 export type HeroIconButtonProps = Omit<ButtonProps, "children"> & {
   icon: IconProps["icon"];
@@ -30,7 +31,10 @@ export const HeroIconButton = ({
     <Button
       ref={buttonRef}
       {...(props as ButtonProps)}
-      className={`${bg}:25 animate-in zoom-in-1/2 h-full flex-shrink cursor-pointer rounded-5 b-none children:inline-block`}
+      className={cls(
+        `${bg}:25`,
+        "h-full flex-shrink animate-in cursor-pointer rounded-5 b-none shadow-white:50 shadow-inset outline-none ring-white:20 transition zoom-in-1/2 children:inline-block active:shadow-xl focus:shadow-2xl hover:shadow-2xl focus:ring-2 hover:ring-2 active:ring-white:50 children:transition children:active:scale-115 children:hover:scale-125"
+      )}
     >
       <Icon icon={icon} variant={variant} size={25} />
     </Button>
