ad18a2b9d8f1f46a6b683391e6110fcd831cb014
Rodolfo Pereira <phinpho@gmail.com>
2024-05-30 09:40:17 +0200

    feat: add the initial landing page site with astro and starlight

---
+++ packages/site/astro.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from 'astro/config';
+import starlight from "@astrojs/starlight";
+import starlightImageZoom from 'starlight-image-zoom';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'static',
+	site: "https://sceneforge.org",
+  integrations: [
+		starlight({
+			plugins: [
+		    starlightImageZoom()
+	    ],
+			title: "Scene Forge",
+			description: "Unleash your creativity with our cutting-edge Progressive Web Application that empowers you to craft, visualise, and manipulate stunning 3D scenes.",
+			logo: {
+				light: "./src/assets/site-header-logo-light.svg",
+				dark: "./src/assets/site-header-logo-dark.svg",
+				replacesTitle: true,
+			},
+			defaultLocale: 'root',
+			locales: {
+			  root: {
+				  label: 'English',
+					lang: 'en',
+				},
+			},
+			sidebar: [
+				{
+					label: "Home",
+					link: "/"
+				},
+				{
+					label: "About",
+					link: "/about",
+				},
+			],
+			customCss: [
+				"./src/styles/custom.css",
+			],
+		}),
+	],
+});
--- packages/site/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { render } from "./root";
-
-try {
-  render();
-}
-catch (error) {
-  throw new Error("Failed to render app", { cause: error });
-}
