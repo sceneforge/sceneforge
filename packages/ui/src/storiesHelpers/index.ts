deb4e12e867f875ce7cd988079c9bfcd1465532e
Rodolfo Pereira <phinpho@gmail.com>
2024-06-25 21:54:14 +0200

    fix(ui): fix storybook helper import and main css

--- packages/ui/src/storiesHelpers/index.ts
+++ packages/ui/src/storiesHelpers/index.ts
@@ -1,4 +1,5 @@
 export { alignArgTypes } from "./alignArgTypes";
+export { iconArgTypes } from "./iconArgTypes";
 export { orientationArgTypes } from "./orientationArgTypes";
 export { positionArgTypes } from "./positionArgTypes";
 export { variantArgTypes } from "./variantArgTypes";
