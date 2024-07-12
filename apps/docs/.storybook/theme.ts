7b4e2f1bb98ddc7eb55a4a391ae9a4dc8efed5fa
Rodolfo Pereira <phinpho@gmail.com>
2024-07-12 21:42:32 +0200

    fix(docs): update relative path for brand url and image

--- apps/docs/.storybook/theme.ts
+++ apps/docs/.storybook/theme.ts
@@ -2,10 +2,10 @@ import { create } from "@storybook/theming";
 
 const theme = create({
   base: "light",
-  brandImage: "/assets/brand-image-light.svg",
+  brandImage: "./assets/brand-image-light.svg",
   brandTarget: "_self",
   brandTitle: "Scene Forge Documentation",
-  brandUrl: "https://app.sceneforge.org/docs",
+  brandUrl: "./",
 });
 
 export default theme;
