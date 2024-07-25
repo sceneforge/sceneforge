7a0ca461454ca7364b359ec824b765fef7a15edb
Rodolfo Pereira <phinpho@gmail.com>
2024-07-25 08:22:12 +0200

    fix(ui): update component exports

--- packages/ui/src/components/ColorPicker/index.ts
+++ packages/ui/src/components/ColorPicker/index.ts
@@ -1,5 +1,11 @@
 import { lazy } from "react";
 
 export type { ColorPickerProps } from "./ColorPicker";
+export type { HueSliderProps } from "./HueSlider";
+export type { LightnessSliderProps } from "./LightnessSlider";
+export type { SaturationSliderProps } from "./SaturationSlider";
 
 export const ColorPicker = lazy(() => import("./ColorPicker"));
+export const HueSlider = lazy(() => import("./HueSlider"));
+export const LightnessSlider = lazy(() => import("./LightnessSlider"));
+export const SaturationSlider = lazy(() => import("./SaturationSlider"));
--- packages/ui/src/components/Field/index.ts
+++ packages/ui/src/components/Field/index.ts
@@ -1,12 +1,14 @@
 import { lazy } from "react";
 
 export const Field = lazy(() => import("./Field"));
+export const FieldColor = lazy(() => import("./FieldColor"));
 export const FieldItem = lazy(() => import("./FieldItem"));
 export const FieldSelect = lazy(() => import("./FieldSelect"));
 export const FieldSwitch = lazy(() => import("./FieldSwitch"));
 export const FieldText = lazy(() => import("./FieldText"));
 
 export type { FieldProps } from "./Field";
+export type { FieldColorProps } from "./FieldColor";
 export type { FieldItemProps } from "./FieldItem";
 export type { FieldSelectProps } from "./FieldSelect";
 export type { FieldSwitchProps } from "./FieldSwitch";
