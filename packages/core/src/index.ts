7857aa88b273d2b251fe146f7204d6b7acb7f68c
Rodolfo Pereira <phinpho@gmail.com>
2024-09-09 12:08:27 +0200

    fix(core): remove all hast package related implementation

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,3 @@
-export * from "./blockquoteTransform";
-export type * from "./blockquoteTransform";
-export * from "./markdown";
 export * from "./openSceneFile";
 export * from "./sceneType";
 export * from "./themes";
